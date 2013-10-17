cb01f25e68c16066cc6368162b2b86b2f51e73a0
James Watterson <jameslwatterson@gmail.com>
2013-10-17 16:54:36 -0400

    HW_11_41 finished

--- HW_11_41.m
+++ HW_11_41.m
@@ -6,7 +6,7 @@ syms s h;
 %create the transfer function
 h(s) = ((R_2/L)*s)/(s^2+(R_1+R_2)*s/L + 1/(L*C));
 %define the frequency range
-w = 0:.1:(3*2*pi/sqrt(L*C));
+w = 0:1:(3*2*pi/sqrt(L*C));
 pretty(h);
 %identify the coefficients of the numerator and denominator
 n = [0 2500 0];
@@ -19,14 +19,20 @@ M = abs(H);
 P = angle(H)*180/pi;
 %Graph
 figure(1)
-plot(M,w)
+semilogx(w,M)
 ylabel('Magnitude');
 xlabel('Frequency');
+grid on;
 figure(2)
-plot(P,w)
+semilogx(w,P)
 ylabel('Phase');
 xlabel('Frequency');
+grid on;
 
 %Part C
 poles = roots(d);
+%Part D
 [maxVal, ind] = max(H);
+%Part E
+halfPower = find(real(H)==.125);
+
